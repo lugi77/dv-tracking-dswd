6fa7477061211c24685f82c764ef676222e450ab
lugi77 <luigillorando7@gmail.com>
2024-09-18 12:13:33 +0800

    program table

---
+++ database/migrations/2024_09_18_023558_create_dv_inventory.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dv_inventory', function (Blueprint $table) {
+            $table->id();
+            $table->string('program')->nullable();
+            $table->integer('no_of_dv')->nullable();
+            $table->decimal('total_amount_program')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dv_inventory');
+    }
+};
