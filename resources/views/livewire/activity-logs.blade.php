9584c26746c5416195749896b8559b15be985b5a
lugi77 <luigillorando7@gmail.com>
2024-09-24 12:46:56 +0800

    minor fix

--- resources/views/livewire/activity-logs.blade.php
+++ resources/views/livewire/activity-logs.blade.php
@@ -22,19 +22,6 @@
                                 <td class="py-3 px-4 border-b border-gray-200 text-blue-800">John Doe</td>
                                 <td class="py-3 px-4 border-b border-gray-200 text-blue-800">Logged in</td>
                             </tr>
-                            <tr class="hover:bg-blue-100 transition duration-200 ease-in-out">
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">2024-09-17 01:15 PM</td>
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">DV-00124</td>
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">Jane Smith</td>
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">Updated profile information
-                                </td>
-                            </tr>
-                            <tr class="hover:bg-blue-100 transition duration-200 ease-in-out">
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">2024-09-17 02:00 PM</td>
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">DV-00125</td>
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">Alice Johnson</td>
-                                <td class="py-3 px-4 border-b border-gray-200 text-blue-800">Deleted record #123</td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
